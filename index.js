1621fb8e7634c6dfe5a4e00cc3282258ace7a9a0
Matti Lankinen <m.lankinen@iki.fi>
2015-07-17 10:40:07 +0300

    Add route configuration

--- index.js
+++ index.js
@@ -3,3 +3,7 @@
 var Routes = require('./lib/routes')
 
 module.exports = new Routes()
+
+module.exports.configure = function configure(opts) {
+  return new Routes(opts)
+}
---
+++ test/configureSpec.js
@@ -0,0 +1,41 @@
+'use strict';
+
+let expect     = require('chai').expect,
+    Q          = require('q'),
+    makeServer = require('./makeServer'),
+    client     = require('./client'),
+    routes     = require('../index')
+
+
+describe('JSON configuration', () => {
+  let server = null
+
+  let customRoutes = routes.configure({
+    errorHandler: (res, error) => res.json({code: error.code})
+  })
+
+  before((done) => {
+    server = makeServer((app) => {
+      app.get('/error', customRoutes.json(() => Q.fcall(() => { throw {code: 401} })))
+    }, done)
+  })
+
+  after((done) => {
+    server.close(done)
+  })
+
+
+  it('supports default value overriding', (done) => {
+    client.get('/error')
+      .get('body')
+      .then(JSON.parse)
+      .then(body => expect(body.code).to.equal(401))
+      .then(bare(done))
+      .done()
+  })
+})
+
+
+function bare(fn) {
+  return function() { fn() }
+}
